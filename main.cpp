bfc1be0dc0764a408d982e97d0784f8ade0b45d6
annabelle <proficiencygit@riseup.net>
2024-10-13 16:01:45 -0700

    archival commit

--- main.cpp
+++ main.cpp
@@ -1,86 +1,92 @@
-#include <stdint.h>
-#include <stdio.h>
 #include <algorithm>
+#include <format>
+#include <print>
+#include <stdint.h>
+#include <string>
 
 // credits: wav
-float normalize( float angle )
+float normalize(float angle)
 {
-	auto revolutions = angle / 360.f;
-	if ( angle > 180.f || angle < -180.f )
-	{
-		revolutions = std::round( std::abs( revolutions ) );
-		if ( angle < 0.f )
-			angle = ( angle + 360.f * revolutions );
-		else
-			angle = ( angle - 360.f * revolutions );
-		return angle;
-	}
-	return angle;
+    auto revolutions = angle / 360.f;
+    if (angle > 180.f || angle < -180.f)
+    {
+        revolutions = std::round(std::abs(revolutions));
+        if (angle < 0.f)
+            angle = (angle + 360.f * revolutions);
+        else
+            angle = (angle - 360.f * revolutions);
+        return angle;
+    }
+    return angle;
 }
 
-float compute_body_pitch( float viewangle )
+/**
+ * @brief computes the angle as the server and hit registration would.
+ * @param viewangle
+ * @return
+ */
+float compute_body_pitch(float viewangle)
 {
-	// purpose: 
-	// creating properly handled angle
-	// as the server, and hit registration
-	// would handle it
-	// this could also be used to show the real angle in thirdperson
-	// example: LocalPlayer->v_angle( ) = ComputeBodyPitch( cmd->viewangles.x
+    // reference:
+    // CBasePlayerAnimState::ComputePoseParam_BodyPitch
+    // https://github.com/ValveSoftware/source-sdk-2013/blob/master/sp/src/game/shared/base_playeranimstate.cpp#L719-L724
 
-	// reference:
-	// CBasePlayerAnimState::ComputePoseParam_BodyPitch
-	// https://github.com/ValveSoftware/source-sdk-2013/blob/master/sp/src/game/shared/base_playeranimstate.cpp#L719-L724
+    if (viewangle > 180.0f)
+        viewangle -= 360.0f;
 
-	if ( viewangle > 180.0f )
-		viewangle -= 360.0f;
-
-	return std::clamp( viewangle, -90.0f, 90.0f );
+    return std::clamp(viewangle, -90.0f, 90.0f);
 }
 
-bool is_fake_pitch( const float& viewangle, const float& goal_fake )
+/**
+ * @brief determines whether or not there's a desync between networked angle and
+ * server's body pitch.
+ * @param viewangle the player's rotation angle.
+ * @param goal_fake
+ * @return
+ */
+bool is_fake_pitch(const float& viewangle, const float& goal_fake)
 {
-	// purpose:
-	// determine if there's a desync between networked player angle 
-	// and body pitch, indicating a fake angle 
+    const float eye_angles = std::abs(normalize(viewangle));
 
-	// usage: see FindSmallestFake
+    if (eye_angles == goal_fake && compute_body_pitch(viewangle) != eye_angles)
+        return true;
 
-	const float eye_angles = std::abs( normalize( viewangle ) );
-
-	if ( eye_angles == goal_fake && compute_body_pitch( viewangle ) != eye_angles )
-		return true;
-
-	return false;
+    return false;
 }
 
-float find_smallest_fake_pitch( const float& goal )
+/**
+ * @brief find viewangle that would produce a fake angle
+ * @param goal 'real' angle
+ * @return required pitch angle to achieve goal 'real' angle
+ */
+float find_fake_pitch(const float& goal)
 {
-	// purpose:
-	// find viewangle that would produce a fake angle
+    // usage:
+    // cmd->viewangles.x = find_fake_pitch(0.0f );
+    // cmd->viewangles.x = find_fake_pitch(180.0f);
 
-	// usage:
-	// cmd->viewangles.x = FindSmallestFake( 0.0f ) 
-	// cmd->viewangles.x = FindSmallestFake( 180.0f ) 
+    for (float angle = 270.0f; angle < 1000.0f; angle += 1.0f)
+    {
+        if (is_fake_pitch(angle, std::abs(goal)))
+        {
+            if (goal < 0.0f)
+                return -angle;
+            else
+                return angle;
+        }
+    }
 
-	for ( float angle = 270.0f; angle < 1000.0f; angle += 1.0f ) // >_>
-	{
-		if ( is_fake_pitch( angle, std::abs( goal ) ) )
-		{
-			if ( goal < 0.0f )
-				return -angle;
-			else
-				return angle;
-		}
-	}
-
-	return std::numeric_limits< float >::max( );
+    return std::numeric_limits<float>::max();
 }
 
-int main( )
+int main()
 {
-	printf( "fake: %.1f\n", find_smallest_fake_pitch( 0.0f ) );
-	printf( "fake: %.1f\n", find_smallest_fake_pitch( 90.0f ) );
-	printf( "fake: %.1f\n", find_smallest_fake_pitch( -90.0f ) );
+    constexpr std::string_view fmt = "Angle: {}. 'Real' angle: {}\n";
 
-	system( "pause" );
-} 
+    for (float goal_x = -90.0f; goal_x <= 180.0f; goal_x += 90.0f)
+    {
+        std::print(fmt.data(), find_fake_pitch(goal_x), goal_x);
+    }
+
+    return 0;
+}
